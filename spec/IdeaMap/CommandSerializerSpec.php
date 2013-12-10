181fe778662280737a0639524709d07506358602
Stewart Duffey <duffey.sj@gmail.com>
2013-12-10 19:45:14 +0000

    Implement decoding from json to command class

---
+++ spec/IdeaMap/CommandSerializerSpec.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace spec\IdeaMap;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CommandSerializerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('IdeaMap\CommandSerializer');
+    }
+
+    function it_should_create_a_command_based_on_the_given_type_string()
+    {
+        $name = 'Test map';
+        $json = json_encode(array('type' => 'CreateMap', 'name' => $name));
+        $cmd = new \IdeaMap\Command\CreateMap(array('name' => $name));
+
+        $this->jsonDecode($json)->shouldBeLike($cmd);
+    }
+
+    function it_should_not_create_a_command_if_the_given_data_is_invalid()
+    {
+        $ex = new \InvalidArgumentException('Could not decode json string to object');
+
+        $this->shouldThrow($ex)->duringJsonDecode('not json');
+    }
+
+    function it_should_not_create_a_command_if_there_is_no_type()
+    {
+        $json = json_encode(array('name' => 'Name'));
+        $ex = new \InvalidArgumentException('Command type not given');
+
+        $this->shouldThrow($ex)->duringJsonDecode($json);
+    }
+
+    function it_should_not_create_a_command_if_the_type_does_not_match_a_command()
+    {
+        $json = json_encode(array('type' => 'InvalidCommand'));
+        $ex = new \InvalidArgumentException('Invalid command type given');
+
+        $this->shouldThrow($ex)->duringJsonDecode($json);
+    }
+
+    // it should not create a command if there is no type
+    // it should not create a command if the type does not match a command
+}
---
+++ src/IdeaMap/CommandSerializer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace IdeaMap;
+
+class CommandSerializer
+{
+    /**
+     *  @param string $json
+     *  @return IdeaMap\Command\Command
+     */
+    public function jsonDecode($json)
+    {
+        $o = json_decode($json);
+        if (!is_object($o)) {
+            throw new \InvalidArgumentException('Could not decode json string to object');
+        }
+
+        if (!isset($o->type)) {
+            throw new \InvalidArgumentException('Command type not given');
+        }
+
+        $className = 'IdeaMap\\Command\\' . $o->type;
+        if (!class_exists($className)) {
+            throw new \InvalidArgumentException('Invalid command type given');
+        }
+
+        unset($o->type);
+
+        return new $className((array) $o);
+    }
+}
