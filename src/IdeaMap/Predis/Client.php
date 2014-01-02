b4dce0535cf1e8a56df7e0299b4384a3e1988425
Stewart Duffey <duffey.sj@gmail.com>
2014-01-02 19:11:47 +0000

    Predis client wrapper

---
+++ src/IdeaMap/Predis/Client.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace IdeaMap\Predis;
+
+use Predis\Client as PredisClient;
+
+class Client
+{
+    protected $predisClient;
+
+    public function __construct(PredisClient $predisClient)
+    {
+        $this->predisClient = $predisClient;
+    }
+
+    public function incr($key)
+    {
+        $this->predisClient->incr($key);
+    }
+
+    public function lpush($key, $value)
+    {
+        $this->predisClient->lpush($key, $value);
+    }
+}
