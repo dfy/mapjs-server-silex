3cb7d454a00cca6e0f09d9b5b4e4ec007d57badf
Stewart Duffey <duffey.sj@gmail.com>
2014-01-21 19:32:34 +0000

    Add AddIdeaToMap command

---
+++ spec/IdeaMap/Command/AddIdeaToMapSpec.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace spec\IdeaMap\Command;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AddIdeaToMapSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(array('id' => 2, 'parentId' => 1));
+    }
+
+    function it_is_initializable_with_an_id_and_parent_id()
+    {
+        $this->shouldHaveType('IdeaMap\Command\AddIdeaToMap');
+    }
+
+    function it_is_not_initializable_without_a_parent_id()
+    {
+        $ex = new \InvalidArgumentException('Parent ID not found in values');
+        $this->shouldThrow($ex)->during('__construct', array(array('id' => 2)));
+    }
+
+    function it_is_not_initializable_without_an_id()
+    {
+        $ex = new \InvalidArgumentException('ID not found in values');
+        $this->shouldThrow($ex)->during('__construct', array(array('parentId' => 1)));
+    }
+
+    function it_is_initializable_with_a_null_parent_id()
+    {
+        $ex = new \InvalidArgumentException('Parent ID not found in values');
+        $this->shouldNotThrow($ex)->during('__construct', array(array('id' => 2, 'parentId' => null)));
+    }
+
+    function it_is_not_initializable_with_a_null_id()
+    {
+        $ex = new \InvalidArgumentException('ID not found in values');
+        $this->shouldThrow($ex)->during('__construct', array(array('id' => null, 'parentId' => 1)));
+    }
+
+    function it_should_give_its_id()
+    {
+        $this->getId()->shouldReturn(2);
+    }
+
+    function it_should_give_its_parents_id()
+    {
+        $this->getParentId()->shouldReturn(1);
+    }
+
+    function it_should_be_json_serializable()
+    {
+        $this->toJson()->shouldReturn('{"type":"AddIdeaToMap","id":2,"parentId":1}');
+    }
+}
+
+/*
+ * fields:
+ * - parent id: nullable if just adding to map
+ * - id: set a session id client side, then assign "real" id server side
+ * -- but how to reconcile old client side id and replacement server side id? new event? does each client need to
+ *    subscribe to a session event feed? overkill for current needs. just include in main feed for now. or handle
+ *    it all client side
+ * - title: the content, just called title in the version I have... not on this command?
+ * - attr: not on this command?
+ * - ideas: not on this command
+ */
---
+++ src/IdeaMap/Command/AddIdeaToMap.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace IdeaMap\Command;
+
+class AddIdeaToMap
+{
+    const TYPE = 'AddIdeaToMap';
+
+    protected $id;
+
+    protected $parentId;
+
+    public function __construct(array $values)
+    {
+        if (!array_key_exists('id', $values) || is_null($values['id'])) {
+            throw new \InvalidArgumentException('ID not found in values');
+        }
+        if (!array_key_exists('parentId', $values)) {
+            throw new \InvalidArgumentException('Parent ID not found in values');
+        }
+
+        $this->id = $values['id'];
+        $this->parentId = $values['parentId'];
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    public function toJson()
+    {
+        return json_encode(
+            (object) array('type' => self::TYPE, 'id' => $this->id, 'parentId' => $this->parentId)
+        );
+    }
+}
